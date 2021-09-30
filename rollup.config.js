cf5aac79f38f5d20fdd70c50fa4f63806f40e832
NuclearRedeye <github@mneah.com>
2021-09-30 08:48:02 +0100

    feat: bundle css for release

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,24 @@
-module.exports = [
-  {
-    input: './out/debug/index.js',
-    output: [
-      {
-        file: './out/release/index.esm.js',
-        format: 'esm',
-        sourcemap: true,
-      },
-      {
-        file: './out/release/index.cjs.js',
-        format: 'cjs',
-        sourcemap: true,
-      }
-    ]
-  }
-];
+import copy from 'rollup-plugin-copy';
+
+export default {
+  input: './out/debug/index.js',
+  output: [
+    {
+      file: './out/release/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      file: './out/release/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    }
+  ],
+  plugins: [
+    copy({
+      targets: [
+        { src: ['./out/debug/index.css'], dest: './out/release/' },
+      ]
+    })
+  ]
+};
